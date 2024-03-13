af02718824653dc5787b89ef6b1e006678914832
Алим Косаев <crazy.kitaiets228@mail.ru>
2024-03-13 22:20:32 +0600

    para

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const restourantsURl = "../db/partners.json";
-const container = document.querySelector('.restoran-wrapper')
+const container = document.querySelector(".restoran-wrapper");
+const wrapper = document.querySelector(".restoran-container");
+const foodRes = document.querySelector(".food-res");
 
 const getData = async (URL) => {
   const res = await fetch(URL, {
@@ -12,33 +14,77 @@ const getData = async (URL) => {
   throw new Error("Ебать ты лох");
 };
 
-const createRestoransCards = async ({name, stars, time_of_delivery, image}) => {
+const createRestoransCards = async ({
+  name,
+  stars,
+  time_of_delivery,
+  image,
+  products,
+}) => {
   const data = await getData(restourantsURl);
-  console.log(data, 'data')
+  console.log(data, "data");
   const card = `
-
-    <li class="restoran-list">
-    <a href="#">
+    <li class="restoran-list" data-product="${products}">
       <div>
         <p class="title">${name}</p>
         <p class="rating">${stars}</p>
         <p class="deliviry">${time_of_delivery}</p>
       </div>
       <div class="image" style="background-image: url('${image}')"></div>
-    </a>
     </li>
 
     `;
 
-    container.insertAdjacentHTML("beforeend", card)
-
+  container.insertAdjacentHTML("beforeend", card);
 };
-createRestoransCards()
+createRestoransCards();
+
+const createFoodCard = ({ name, price, description, image }) => {
+  const foodCard = `
+      <li class="food-card">
+      <div>
+        <p class="title">${name}</p>
+        <p class="rating">${price}</p>
+        <p class="description">${description}</p>
+        <button class="buy">BUY</button>
+      </div>
+      <div class="image" style="background-image: url('${image}')"></div>
+    </li>
+  `;
+
+  container.classList.add("hide");
+
+  wrapper.insertAdjacentHTML("beforeend", foodCard);
+};
+
+const openRes = async (e) => {
+  const target = e.target;
+  const restoran = target.closest(".restoran-list");
+  if (restoran) {
+    const restData = await getData(`./db/${restoran.dataset.product}`);
+    wrapper.innerHTML = "";
+    restData.map((x) => createFoodCard(x));
+    wrapper.classList.remove("hide");
+  }
+};
 
 const init = async () => {
-  const resaurantsArr = await getData(restourantsURl)
-  
-  resaurantsArr.map((x) => createRestoransCards(x))
-}
+  const resaurantsArr = await getData(restourantsURl);
+  resaurantsArr.map((x) => createRestoransCards(x));
+};
 
-init()
+container.addEventListener("click", openRes);
+
+const goBack = async (e) => {
+  const card = e.target.closest(".backBtn");
+  if (card) {
+    container.classList.remove("hide");
+    wrapper.classList.add("hide");  
+    wrapper.innerHTML = "";
+  }
+
+};
+
+foodRes.addEventListener("click", goBack);
+
+init();
